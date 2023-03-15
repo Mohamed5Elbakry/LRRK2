4535375ec68081f2f1b104d311f0e18ff56fe7e2
Mohamed Elbakry <104537016+Mohamed5Elbakry@users.noreply.github.com>
2023-03-15 19:46:06 +0200

    Update 2_Virtual Screening.py

--- pages/2_Virtual Screening.py
+++ pages/2_Virtual Screening.py
@@ -73,7 +73,7 @@ uplouded_file=st.file_uploader("Please upload your input file", type=['txt'])
 
 if st.button('Predict'):
     reading_data = pd.read_table(uplouded_file, sep=' ', header=None)
-    reading_data.to_csv('molecule.smi', sep = '   ', index = False,header=None)
+    reading_data.to_csv('molecule.smi', sep = '\t', index = False,header=None)
     st.subheader('The input data')
     st.write(reading_data)
 
